7ac2f67197eba9708a949b3899f26826bd949fbe
sonutwo22-lab <sonutwo22@gmail.com>
2026-06-24 17:06:24 +0530

    Migrating to official Tailwind v4 Vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // This tells Vite where to put the final files
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
