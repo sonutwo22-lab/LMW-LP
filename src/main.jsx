727a3d73a9935471d25518be44af95fc6ea8ffb5
sonutwo22-lab <sonutwo22@gmail.com>
2026-06-24 17:00:23 +0530

    Fixing className and Tailwind v4 import

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import './index.css'; // This must be here!
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This tells Vite where to put the final files
+  },
 })
